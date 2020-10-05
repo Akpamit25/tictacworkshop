c01437197a0bc47ca353b1b4f9480825d3a0c49f
Amit Kumar <amitnitsri25@gmail.com>
2020-10-05 13:04:01 +0530

    UC3

--- src/com/Tictac/TicTacToeGame.java
+++ src/com/Tictac/TicTacToeGame.java
@@ -1,18 +1,53 @@
+
 package com.Tictac;
+
 import java.util.*;
 
 public class TicTacToeGame {
 
-	char[][]board = new char [3][3];
-	public void display()
+	private static char[] createBoard() // Createboard-UC1
 	{
-		System.out.println("Board Created"); //printing on the console
+		char board[] = new char[10];
+		for (int i = 0; i < board.length; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
+
+	private static char[] playerInput() // Createboard-UC2
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose Either X OR O");
+		char input = sc.next().charAt(0);
+		char userInput = ' ';
+		char computerInput = ' ';
+
+		if (input == 'X') {
+			userInput = input;
+			computerInput = '0';
+		} else if (input == 'O') {
+			userInput = input;
+			computerInput = 'X';
+		}
+
+		char[] output = { userInput, computerInput };
+		return output;
+	}
+
+	private void showBoard(char[] board) {
+		System.out.println("Output:");
+		for (int i = 0; i < board.length; i++) {
+			System.out.println("[" + board[i] + "]");
+		}
+
 	}
 	
-	
-public static void main (String args[])
-{
-	TicTacToeGame tictacobj = new TicTacToeGame();
-	tictacobj.display();
-}
+	public static void main(String args[]) {
+		char[] board = createBoard();
+		TicTacToeGame sample = new TicTacToeGame();
+		System.out.println("Enter your mark");
+		char[] storing = sample.playerInput();
+		sample.showBoard(board);
+
+	}
 }
